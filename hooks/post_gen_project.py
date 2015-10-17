9ac8227432b6e999a0d9ed97752248683915f79d
thatarchguy <thatarchguy@users.noreply.github.com>
2015-10-17 00:20:14 -0400

    Changed to use builtin module

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -4,7 +4,7 @@ from __future__ import unicode_literals, absolute_import, print_function
 import os
 import shutil
 from collections import OrderedDict
-from cookiecutter.prompt import query_yes_no
+from cookiecutter.prompt import read_user_yes_no
 
 try:
     input = raw_input
@@ -52,7 +52,7 @@ folders['files'] = {
 def configure_role():
     print('\n\nROLE CONFIGURATION:\n===================')
     for folder_name, folder in folders.items():
-        if query_yes_no(folder['question']):
+        if read_user_yes_no(folder['question'], default_value=u'yes'):
 
             try:
                 # this file has to be there, git doesn't store empty folders.
@@ -64,7 +64,7 @@ def configure_role():
                 with open('{}/main.yml'.format(folder_name), 'a') as fp:
 
                     if 'pre_hint' in folder:
-                        if query_yes_no(folder['pre_hint']):
+                        if read_user_yes_no(folder['pre_hint'], default_value=u'yes'):
                             fp.write(folder['pre_action'])
                         else:
                             continue
